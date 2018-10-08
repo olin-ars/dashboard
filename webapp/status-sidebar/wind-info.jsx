a6ca3f017634eea7e1472c7f8ff2010348c7ae42
Kyle Combes <kylecombes@gmail.com>
2018-10-08 09:50:10 -0400

    Add toggle for relative and true wind

--- webapp/status-sidebar/wind-info.jsx
+++ webapp/status-sidebar/wind-info.jsx
@@ -8,15 +8,35 @@ export default class WindInfo extends React.Component {
 
     this.state = {
       spoofWind: false,
+      windType: 'windRel',
     };
   }
 
+  windTypeChanged = (e) => {
+    this.setState({ windType: e.target.value });
+  };
+
   render() {
     const {
+      windRel,
       windAbs,
       spoofedWindChanged,
     } = this.props;
-    const { spoofWind } = this.state;
+    const {
+      spoofWind,
+      windType,
+    } = this.state;
+
+    const windData = windType === 'windRel' ? windRel : windAbs;
+
+    const windTypeSelector = (
+      <div>
+        <input type="radio" onChange={this.windTypeChanged} name="windType" value="windAbs" id="trueWind" checked={windType === 'windAbs'} />
+        <label htmlFor="trueWind">True</label>
+        <input type="radio" onChange={this.windTypeChanged} name="windType" value="windRel" id="relWind" checked={windType === 'windRel'} />
+        <label htmlFor="relWind">Relative</label>
+      </div>
+    );
 
     const checkbox = (
       <label>
@@ -27,12 +47,13 @@ export default class WindInfo extends React.Component {
 
     return spoofWind ? (
       <div className="wind-info">
+        {windTypeSelector}
         {checkbox}
         <StatText title="Direction">
           <input
             type="text"
             name="theta"
-            value={windAbs.direction}
+            value={windData.direction}
             onChange={spoofedWindChanged}
           />
         </StatText>
@@ -40,20 +61,21 @@ export default class WindInfo extends React.Component {
           <input
             type="text"
             name="x"
-            value={windAbs.speed}
+            value={windData.speed}
             onChange={spoofedWindChanged}
           />
         </StatText>
       </div>
     ) : (
       <div className="wind-info">
+        {windTypeSelector}
         {checkbox}
         <StatText title="Direction">
-          {windAbs.direction}
+          {windData.direction}
           &deg;
         </StatText>
         <StatText title="Speed">
-          {windAbs.speed}
+          {windData.speed}
           &nbsp;
           knots
         </StatText>
@@ -63,6 +85,10 @@ export default class WindInfo extends React.Component {
 }
 
 WindInfo.propTypes = {
+  windRel: PropTypes.shape({
+    direction: PropTypes.number.isRequired,
+    speed: PropTypes.number.isRequired,
+  }).isRequired,
   windAbs: PropTypes.shape({
     direction: PropTypes.number.isRequired,
     speed: PropTypes.number.isRequired,
